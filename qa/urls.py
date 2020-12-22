b9fab78ec3f2d6c26c9ac7e5915e274faa79b82f
ben <benmeehan111@gmail.com>
2020-12-22 15:07:33 +0530

    java quiz

--- qa/urls.py
+++ qa/urls.py
@@ -3,11 +3,14 @@ from qa import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('initial/', views.initial, name="index"),
-    path('get_users_py/', views.get_users_py, name="index"),
-    path('get_users_cn/', views.get_users_cn, name="index"),
+    path('initial/', views.initial, name="initial"),
+    path('get_users_py/', views.get_users_py, name="userspy"),
+    path('get_users_cn/', views.get_users_cn, name="userscn"),
+    path('get_users_java/', views.get_users_java, name="usersjava"),
     path('python/', views.python, name='python'),
-    path('cn/', views.networks, name='python'),
-    path('submit_python/', views.submit_python, name='python'),
-    path('submit_networks/', views.submit_networks, name='python'),
+    path('cn/', views.networks, name='networks'),
+    path('java/', views.java, name='java'),
+    path('submit_python/', views.submit_python, name='subpython'),
+    path('submit_networks/', views.submit_networks, name='subnetworks'),
+    path('submit_java/', views.submit_java, name='subjava'),
 ]
--- qa/views.py
+++ qa/views.py
@@ -6,6 +6,27 @@ from qa.models import Question
 
 python_users = 0
 network_users = 0
+java_users = 0
+
+
+def question_generator(term):
+    ques = list(Question.objects.all())
+    mydict = {}
+    final_dict = {}
+    numbering = 0
+    for i in range(len(ques)):
+        if(ques[i].cat == term):
+            mydict['ques'] = ques[i].ques
+            mydict['ans1'] = ques[i].ans1
+            mydict['ans2'] = ques[i].ans2
+            mydict['ans3'] = ques[i].ans3
+            mydict['ans4'] = ques[i].ans4
+            mydict['correct'] = ques[i].correct_answer
+            final_dict['question'+str(numbering)] = mydict
+            numbering += 1
+
+        mydict = {}
+    return final_dict
 
 
 def index(req):
@@ -23,26 +44,13 @@ def get_users_py(req):
 def get_users_cn(req):
     return HttpResponse(network_users)
 
+def get_users_java(req):
+    return HttpResponse(java_users)
+
 
 def python(req):
     global python_users
-    ques = list(Question.objects.all())
-    mydict = {}
-    final_dict = {}
-    numbering = 0
-
-    for i in range(len(ques)):
-        if(ques[i].cat == 'python'):
-            mydict['ques'] = ques[i].ques
-            mydict['ans1'] = ques[i].ans1
-            mydict['ans2'] = ques[i].ans2
-            mydict['ans3'] = ques[i].ans3
-            mydict['ans4'] = ques[i].ans4
-            mydict['correct'] = ques[i].correct_answer
-            final_dict['question'+str(numbering)] = mydict
-            numbering += 1
-
-        mydict = {}
+    final_dict = question_generator('python')
     python_users += 1
     final_dict['users'] = python_users
     return JsonResponse(final_dict)
@@ -50,24 +58,17 @@ def python(req):
 
 def networks(req):
     global network_users
-    ques = list(Question.objects.all())
-    mydict = {}
-    final_dict = {}
-    numbering = 0
-    for i in range(len(ques)):
-        if(ques[i].cat == 'cn'):
-            mydict['ques'] = ques[i].ques
-            mydict['ans1'] = ques[i].ans1
-            mydict['ans2'] = ques[i].ans2
-            mydict['ans3'] = ques[i].ans3
-            mydict['ans4'] = ques[i].ans4
-            mydict['correct'] = ques[i].correct_answer
-            final_dict['question'+str(numbering)] = mydict
-            numbering += 1
-
-        mydict = {}
+    final_dict = question_generator('cn')
     network_users += 1
     final_dict['users'] = network_users
+    return JsonResponse(final_dict)
+
+
+def java(req):
+    global java_users
+    final_dict = question_generator('java')
+    java_users += 1
+    final_dict['users'] = java_users
     return JsonResponse(final_dict)
 
 
@@ -81,3 +82,9 @@ def submit_networks(req):
     global network_users
     network_users -= 1
     return HttpResponse(network_users)
+
+
+def submit_java(req):
+    global java_users
+    java_users -= 1
+    return HttpResponse(java_users)
